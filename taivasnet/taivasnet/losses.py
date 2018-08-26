Fixed MSE loss and gradient to work correctly
Fixed problems in MSE gradient calculation and finally managed to
properly unit test it.

Also updated documentation and added better defaults train-linear.py.

taivasnet/taivasnet/losses.py
@@ -45,6 +45,8 @@ class CrossEntropy():
 class MSE():
     """
     Mean Squared Error
+
+    https://en.wikipedia.org/wiki/Mean_squared_error
     """
 
     def loss(self, y_pred, y):
@@ -57,5 +59,4 @@ class MSE():
         return np.sum((y_pred - y)**2) / y.size
 
     def gradient(self, y_pred, y, inputs):
-        k = y_pred.shape[0]
-        return (y_pred - y) / y.size
+        return 2*(y_pred - y) / y.size

taivasnet/test/test_losses.py
@@ -82,30 +82,14 @@ class TestMSE(unittest.TestCase):
 
         loss = self.mse.loss(y_pred, y)
         self.assertTrue(np.isclose(loss, correct, rtol=1e-4))
+        pass
 
     def test_gradient(self):
-        epsilon = 1e-1
+        epsilon = 1e-12
+        y_pred = np.random.randn(5, 5)
+        y = np.random.randn(5, 5)
 
-        inputs = np.array([[1.0], [2.0], [3.0]])
-        targets = np.multiply(inputs, 2)
+        grad_numerical = GradientChecker.eval_numerical_gradient(lambda x: self.mse.loss(y_pred, y), y_pred, verbose=False)
+        grad = self.mse.gradient(y_pred, y, 1)
 
-        np.random.seed(1)
-        linear = Linear(1, 1)
-
-        def f(x):
-            predictions = linear.forward(x)
-            return self.mse.loss(predictions, targets)
-
-        grad_output = np.ones((3, 1))
-        grad_numerical = GradientChecker.eval_numerical_gradient_array(f, inputs, grad_output)
-
-        # for some reason the numerical gradients are almost exactly ten times larger,
-        # haven't figured the exact reason for this yet
-        grad_numerical = np.divide(grad_numerical, 10)
-
-        predictions = linear.forward(inputs)
-
-        grad_inputs, grad_weights, grad_bias = linear.backward(grad_output)
-        grad_inputs = self.mse.gradient(predictions, targets, inputs)
-
-        self.assertTrue(np.allclose(grad_inputs, grad_numerical, rtol=epsilon), msg="MSE gradient works")
+        self.assertTrue(np.allclose(grad, grad_numerical, rtol=epsilon), msg="MSE gradient works")